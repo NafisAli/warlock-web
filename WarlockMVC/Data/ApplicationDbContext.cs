19f3379cf1a73eadf508bcb7088dcce9a42fbcd9
nafis.exe <nafis.ali0123@gmail.com>
2024-07-14 03:41:35 +1000

    Add ApplicationDbContext for Entity Framework
    A new ApplicationDbContext class has been added to the project. This class inherits from DbContext and will be used as the primary means of interacting with the database using Entity Framework. The constructor accepts DbContextOptions, allowing for configuration flexibility.

---
+++ WarlockMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WarlockMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
