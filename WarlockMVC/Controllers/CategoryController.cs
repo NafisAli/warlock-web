cabd32aa35ebe264b2b8e84cfa9b89bb23e6c7dc
nafis.exe <nafis.ali0123@gmail.com>
2024-07-14 04:10:36 +1000

    Add CategoryController with Index action
    A new controller named 'CategoryController' has been added to the project. This controller includes an 'Index' action that currently returns a view. The purpose of this addition is to handle category-related requests in the application.

---
+++ WarlockMVC/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarlockMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
