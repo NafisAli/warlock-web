2543732d2b9d3f22e4f5251649d099f314181f7e
nafis.exe <nafis.ali0123@gmail.com>
2024-07-14 03:20:01 +1000

    Add new Category model
    A new model named 'Category' has been added. This model includes properties for Id, Name, and DisplayOrder.

---
+++ WarlockMVC/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WarlockMVC.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+    }
+}
