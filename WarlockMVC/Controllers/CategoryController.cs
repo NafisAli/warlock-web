d18160e579b65f428bd84547db7db4299112e0d9
nafis.exe <nafis.ali0123@gmail.com>
2024-07-14 04:23:08 +1000

    Add database context to CategoryController
    The CategoryController now uses the ApplicationDbContext for data access. The constructor has been updated to accept an instance of ApplicationDbContext, which is then used in the Index action method to retrieve a list of categories from the database and pass it to the view.

--- WarlockMVC/Controllers/CategoryController.cs
+++ WarlockMVC/Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WarlockMVC.Data;
+using WarlockMVC.Models;
 
 namespace WarlockMVC.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+
+            return View(objCategoryList);
         }
     }
 }
