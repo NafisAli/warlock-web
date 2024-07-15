e0114a11115b58e5bda630af4ef9eadb6a99dd31
nafis.exe <nafis.ali0123@gmail.com>
2024-07-16 02:47:29 +1000

    Add seed data to Products table
    This update includes the addition of seed data to the Products table. The new migration file contains methods for both inserting and deleting this data. The inserted data includes various product details such as Id, Description, Level, ListPrice, Price, Price100, Price50, Tier and Title. This change also reflects in the ApplicationDbContextModelSnapshot file where the model builder now has this seeded data.

---
+++ Warlock.DataAccess/Migrations/20240715164651_SeedProductTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Warlock.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Level", "ListPrice", "Price", "Price100", "Price50", "Tier", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 1, 99.0, 90.0, 80.0, 85.0, "Common", "Fireball" },
+                    { 2, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 2, 40.0, 30.0, 20.0, 25.0, "Normal", "Healing Potion" },
+                    { 3, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 3, 55.0, 50.0, 35.0, 40.0, "Normal", "Wooden Staff" },
+                    { 4, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 4, 70.0, 65.0, 55.0, 60.0, "Epic", "Steel Dagger" },
+                    { 5, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 5, 300.0, 270.0, 200.0, 250.0, "Legendary", "Rock in the Ocean" },
+                    { 6, "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ", 6, 250.0, 230.0, 200.0, 220.0, "Legendary", "Leaves and Wonders" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
