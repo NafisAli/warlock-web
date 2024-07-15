57f2414619e8c791c80283ace08930dc5956a28b
nafis.exe <nafis.ali0123@gmail.com>
2024-07-16 02:08:17 +1000

    Update default routing pattern with area
    The default routing pattern in the MVC application has been updated. Previously, it was set to "{controller=Home}/{action=Index}/{id?}", but now it includes an area parameter and is changed to "{area=Customer}/{controller=Home}/{action=Index}/{id?}". This change allows for better organization of controllers and views based on their functional areas.

--- WarlockMVC/Program.cs
+++ WarlockMVC/Program.cs
@@ -33,7 +33,7 @@ internal class Program
 
         app.MapControllerRoute(
             name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
+            pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
         app.Run();
     }
