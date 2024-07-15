d6a4ebe27e98a6f72647c669a270a06d3ce56339
nafis.exe <nafis.ali0123@gmail.com>
2024-07-16 02:10:24 +1000

    Move Home and Privacy to Customer area

--- WarlockMVC/Areas/Customer/Controllers/HomeController.cs
+++ WarlockMVC/Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Warlock.Models;
 
-namespace WarlockMVC.Controllers
+namespace WarlockMVC.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
