656e97945f417cd81669675d121399c287617777
nafis.exe <nafis.ali0123@gmail.com>
2024-07-29 09:09:56 +1000

    Update product retrieval in admin controller
    The product retrieval method in the admin area has been updated to include related category data. This change optimizes the process by reducing the number of database queries, as it now fetches all necessary information in a single call.

--- WarlockMVC/Areas/Admin/Controllers/ProductController.cs
+++ WarlockMVC/Areas/Admin/Controllers/ProductController.cs
@@ -23,7 +23,9 @@ namespace WarlockMVC.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> objProductList = _unitOfWork
+                .Product.GetAll(includeProperties: "Category")
+                .ToList();
 
             return View(objProductList);
         }
