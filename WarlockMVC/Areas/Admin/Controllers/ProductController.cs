d173c93d3d0a3cd952384b9942ffc3abe3c6d3a1
nafis.exe <nafis.ali0123@gmail.com>
2024-07-29 07:58:26 +1000

    Add image handling to ProductController
    The ProductController has been updated to handle image uploads. The constructor now takes an additional IWebHostEnvironment parameter, which is used to get the root path of the web host. When a new product is added, if an image file is provided, it's saved in the 'images\product' directory under the web root path with a unique filename. The URL of this image is then stored in the product's ImageUrl property before updating it in the database.

--- WarlockMVC/Areas/Admin/Controllers/ProductController.cs
+++ WarlockMVC/Areas/Admin/Controllers/ProductController.cs
@@ -13,10 +13,12 @@ namespace WarlockMVC.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -60,7 +62,27 @@ namespace WarlockMVC.Areas.Admin.Controllers
             if (!ModelState.IsValid)
                 return View();
 
-            _unitOfWork.Product.Add(productVM.Product);
+            string wwwRootPath = _webHostEnvironment.WebRootPath;
+
+            if (file != null)
+            {
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                using (
+                    var fileStream = new FileStream(
+                        Path.Combine(productPath, fileName),
+                        FileMode.Create
+                    )
+                )
+                {
+                    file.CopyTo(fileStream);
+                }
+
+                productVM.Product.ImageUrl = @"\images\product\" + fileName;
+            }
+
+            _unitOfWork.Product.Update(productVM.Product);
             _unitOfWork.Save();
 
             TempData["success"] = "Product created successfully";
