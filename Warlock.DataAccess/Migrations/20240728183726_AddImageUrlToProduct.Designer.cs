fb2b2ed3f6b3f60bc30eb6351899af6b537812af
nafis.exe <nafis.ali0123@gmail.com>
2024-07-29 04:38:49 +1000

    Add ImageUrl field to Products table
    A new migration has been added that introduces an 'ImageUrl' field to the 'Products' table. This is a nullable string type column. The existing data in the 'Products' table has been updated to set this new column's value as null for all current entries. In case of rollback, the migration will remove the 'ImageUrl' column from the 'Products' table. The model snapshot has also been updated to reflect this change in schema.

--- Warlock.DataAccess/Migrations/20240728183726_AddImageUrlToProduct.Designer.cs
+++ Warlock.DataAccess/Migrations/20240728183726_AddImageUrlToProduct.Designer.cs
@@ -2,17 +2,20 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Warlock.DataAccess.Data;
 
 #nullable disable
 
-namespace WarlockMVC.Migrations
+namespace Warlock.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240728183726_AddImageUrlToProduct")]
+    partial class AddImageUrlToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
