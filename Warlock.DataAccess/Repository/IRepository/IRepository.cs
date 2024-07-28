1ea0e0e6f7d692e4e43f203d365d3507e24ac596
nafis.exe <nafis.ali0123@gmail.com>
2024-07-29 09:09:35 +1000

    Update IRepository interface methods
    The IRepository interface has been updated to include an optional 'includeProperties' parameter in the 'GetAll' and 'Get' methods. This allows for more flexible querying by optionally including related entities in the results.

--- Warlock.DataAccess/Repository/IRepository/IRepository.cs
+++ Warlock.DataAccess/Repository/IRepository/IRepository.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 
 namespace Warlock.DataAccess.Repository.IRepository
 {
-    public interface IRepository<T> where T : class
+    public interface IRepository<T>
+        where T : class
     {
         // T - Category
-        IEnumerable<T> GetAll();
-        T? Get(Expression<Func<T, bool>> filter);
+        IEnumerable<T> GetAll(string? includeProperties = null);
+        T? Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
         void Add(T entity);
         void Delete(T entity);
         void DeleteRange(IEnumerable<T> entities);
