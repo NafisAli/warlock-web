acc176c737e2108ba0d11bf5abafb7227699cdc4
nafis.exe <nafis.ali0123@gmail.com>
2024-07-29 08:46:12 +1000

    Add image deletion and product update logic
    The code changes introduce a check for existing images when updating a product. If an old image exists, it is deleted before the new one is saved. Additionally, the logic for saving products has been updated to differentiate between creating a new product and updating an existing one. The appropriate success message is now displayed based on whether a product was created or updated.

--- WarlockMVC/Areas/Admin/Controllers/ProductController.cs
+++ WarlockMVC/Areas/Admin/Controllers/ProductController.cs
@@ -69,6 +69,19 @@ namespace WarlockMVC.Areas.Admin.Controllers
                 string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                 string productPath = Path.Combine(wwwRootPath, @"images\product");
 
+                if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                {
+                    var oldImagePath = Path.Combine(
+                        wwwRootPath,
+                        productVM.Product.ImageUrl.TrimStart('\\')
+                    );
+
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+
                 using (
                     var fileStream = new FileStream(
                         Path.Combine(productPath, fileName),
@@ -82,10 +95,18 @@ namespace WarlockMVC.Areas.Admin.Controllers
                 productVM.Product.ImageUrl = @"\images\product\" + fileName;
             }
 
-            _unitOfWork.Product.Update(productVM.Product);
-            _unitOfWork.Save();
+            if (productVM.Product.Id == 0)
+            {
+                _unitOfWork.Product.Add(productVM.Product);
+                TempData["success"] = "Product created successfully";
+            }
+            else
+            {
+                _unitOfWork.Product.Update(productVM.Product);
+                TempData["success"] = "Product updated successfully";
+            }
 
-            TempData["success"] = "Product created successfully";
+            _unitOfWork.Save();
 
             return RedirectToAction("Index");
         }
