24be490c79895d6c608a673d0eda75bfdee81edf
nafis.exe <nafis.ali0123@gmail.com>
2024-07-29 04:35:20 +1000

    Refactor Product model and add Category relationship
    The Product model has been refactored to improve readability by adding line breaks between properties. Additionally, a new property 'CategoryId' has been introduced along with a foreign key relationship to the Category model. This change allows each product to be associated with a specific category, enhancing data organization and retrieval capabilities.

--- Warlock.Models/Product.cs
+++ Warlock.Models/Product.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,28 +13,40 @@ namespace Warlock.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         public required string Title { get; set; }
         public string? Description { get; set; }
+
         [Required]
         public required int Level { get; set; }
+
         [Required]
         public required string Tier { get; set; }
+
         [Required]
         [Display(Name = "List Price")]
         [Range(1, 100)]
         public required double ListPrice { get; set; }
+
         [Required]
         [Display(Name = "Price for 1-50")]
         [Range(1, 100)]
         public required double Price { get; set; }
+
         [Required]
         [Display(Name = "Price for 50+")]
         [Range(1, 100)]
         public required double Price50 { get; set; }
+
         [Required]
         [Display(Name = "Price for 100+")]
         [Range(1, 100)]
         public required double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
     }
 }
