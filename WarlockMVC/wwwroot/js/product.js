7d077b6d022d740ac30625217e65999bc8b712a2
nafis.exe <nafis.ali0123@gmail.com>
2024-07-29 10:22:59 +1000

    Add product data table functionality
    A new JavaScript file has been added to handle the product data table. This includes a function to load the DataTable with specific column widths and AJAX URL for fetching data. It also provides 'Edit' and 'Delete' buttons for each row, allowing users to modify or remove entries directly from the table.

---
+++ WarlockMVC/wwwroot/js/product.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    $('#tblData').DataTable({
+        "ajax": { url:  '/admin/product/getall' },
+        "columns": [
+            { data: 'title', "width": "20%" },
+            { data: 'level', "width": "5%" },
+            { data: 'tier', "width": "10%" },
+            { data: 'listPrice', "width": "10%" },
+            { data: 'category.name', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                      <a href="/admin/product/upsert?=${data}" class="btn btn-primary mx-1"><i class="bi bi-pencil-square"></i> Edit</a>
+                      <a href="/admin/product/delete/${data}" class="btn btn-danger "><i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                },
+                "width": "20%"
+             }
+        ]
+    });
+}
