573b1f32336a71f6b7b6fa47bc2e0e8a1d389a6e
nafis.exe <nafis.ali0123@gmail.com>
2024-08-04 02:49:45 +1000

    Add custom paths for authentication cookies
    The commit introduces custom paths for login, logout, and access denied scenarios in the application cookie configuration. This change allows more control over the routing during these events.

--- WarlockMVC/Program.cs
+++ WarlockMVC/Program.cs
@@ -28,6 +28,12 @@ internal class Program
             )
             .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultTokenProviders();
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.LoginPath = $"/Identity/Account/Login";
+            options.LogoutPath = $"/Identity/Account/Logout";
+            options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+        });
         builder.Services.AddScoped<IEmailSender, EmailSender>();
         builder.Services.AddRazorPages();
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
