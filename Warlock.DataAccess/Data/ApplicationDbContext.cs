d4b3fc336230b0cee524d7b9350dea7828f00e3f
nafis.exe <nafis.ali0123@gmail.com>
2024-08-06 23:54:52 +1000

    Add Faction DbSet to ApplicationDbContext
    A new DbSet for Factions has been added to the ApplicationDbContext. This change allows us to interact with the Faction entity through Entity Framework, enabling CRUD operations on this entity.

--- Warlock.DataAccess/Data/ApplicationDbContext.cs
+++ Warlock.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Warlock.Models;
@@ -12,6 +12,7 @@ namespace Warlock.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Faction> Factions { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
