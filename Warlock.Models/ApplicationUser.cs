1f0c61216d0288dc5722d7772086c777dade2198
nafis.exe <nafis.ali0123@gmail.com>
2024-08-07 00:29:17 +1000

    Add Faction field to ApplicationUser model
    Added a new property 'Faction' to the ApplicationUser model. This includes a ForeignKey attribute linked to 'FactionId', and a ValidateNever attribute to prevent validation on this field. Also, added necessary using directives for these changes.

--- Warlock.Models/ApplicationUser.cs
+++ Warlock.Models/ApplicationUser.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace Warlock.Models
 {
@@ -17,5 +19,10 @@ namespace Warlock.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostCode { get; set; }
+        public int? FactionId { get; set; }
+
+        [ForeignKey("FactionId")]
+        [ValidateNever]
+        public Faction? Faction { get; set; }
     }
 }
