3dd348e757019bb6cc73a22b69747bb3848139e3
nafis.exe <nafis.ali0123@gmail.com>
2024-08-06 23:54:11 +1000

    Add FactionRepository for data access
    A new repository, FactionRepository, has been added to handle data operations related to the 'Faction' model. This repository extends the base Repository class and implements the IFactionRepository interface. It includes a constructor that takes an ApplicationDbContext instance and an Update method for updating Faction objects in the database.

---
+++ Warlock.DataAccess/Repository/FactionRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Warlock.DataAccess.Data;
+using Warlock.DataAccess.Repository.IRepository;
+using Warlock.Models;
+
+namespace Warlock.DataAccess.Repository
+{
+    public class FactionRepository : Repository<Faction>, IFactionRepository
+    {
+        private ApplicationDbContext _db;
+
+        public FactionRepository(ApplicationDbContext db)
+            : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Faction obj)
+        {
+            _db.Factions.Update(obj);
+        }
+    }
+}
