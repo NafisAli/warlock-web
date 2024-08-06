c7260bac0209212b5fdc1b8ab0f76dbf17b47b38
nafis.exe <nafis.ali0123@gmail.com>
2024-08-06 23:59:11 +1000

    Fix URL parameter in product edit button
    The URL parameter for the product edit button was incorrectly formatted. This has been corrected to ensure that the correct product ID is passed when editing a product.

--- WarlockMVC/wwwroot/js/product.js
+++ WarlockMVC/wwwroot/js/product.js
@@ -17,7 +17,7 @@ function loadDataTable() {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                      <a href="/admin/product/upsert?=${data}" class="btn btn-primary mx-1"><i class="bi bi-pencil-square"></i> Edit</a>
+                      <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-1"><i class="bi bi-pencil-square"></i> Edit</a>
                       <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger"><i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
                 },
