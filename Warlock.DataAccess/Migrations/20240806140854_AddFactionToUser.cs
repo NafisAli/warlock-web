efeac3ea3752c57758795994cb5d2556c6c9149f
nafis.exe <nafis.ali0123@gmail.com>
2024-08-07 00:33:11 +1000

    Add FactionId to AspNetUsers table
    This commit introduces a new column, FactionId, to the AspNetUsers table. This change includes creating an index for the new column and setting up a foreign key relationship with the Factions table. The migration files have been updated accordingly. In addition, the ApplicationDbContextModelSnapshot has been modified to reflect these changes in the model structure.

---
+++ Warlock.DataAccess/Migrations/20240806140854_AddFactionToUser.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Warlock.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFactionToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FactionId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_FactionId",
+                table: "AspNetUsers",
+                column: "FactionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Factions_FactionId",
+                table: "AspNetUsers",
+                column: "FactionId",
+                principalTable: "Factions",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Factions_FactionId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_FactionId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FactionId",
+                table: "AspNetUsers");
+        }
+    }
+}
