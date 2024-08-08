3d6f95c1484e67c8826733d15c473ccaf3507de3
nafis.exe <nafis.ali0123@gmail.com>
2024-08-09 09:59:30 +1000

    Add ApplicationUserRepository class
    A new repository class, ApplicationUserRepository, has been added. This class extends the base Repository and implements IApplicationUserRepository. It includes a constructor that takes an ApplicationDbContext instance as a parameter.

---
+++ Warlock.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Warlock.DataAccess.Data;
+using Warlock.DataAccess.Repository.IRepository;
+using Warlock.Models;
+
+namespace Warlock.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext db)
+            : base(db)
+        {
+            _db = db;
+        }
+    }
+}
