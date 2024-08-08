517a585248e0a2db6d3bd4b4b45e07510e0c3c8d
nafis.exe <nafis.ali0123@gmail.com>
2024-08-09 09:55:52 +1000

    Update IRepository interface methods
    The IRepository interface has been updated to enhance the flexibility of data retrieval. The 'GetAll' method now accepts an optional filter parameter, allowing for more specific queries. Additionally, a 'tracked' parameter has been added to the 'Get' method, providing control over entity tracking.

--- Warlock.DataAccess/Repository/IRepository/IRepository.cs
+++ Warlock.DataAccess/Repository/IRepository/IRepository.cs
@@ -10,9 +10,15 @@ namespace Warlock.DataAccess.Repository.IRepository
     public interface IRepository<T>
         where T : class
     {
-        // T - Category
-        IEnumerable<T> GetAll(string? includeProperties = null);
-        T? Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>>? filter = null,
+            string? includeProperties = null
+        );
+        T? Get(
+            Expression<Func<T, bool>> filter,
+            string? includeProperties = null,
+            bool tracked = false
+        );
         void Add(T entity);
         void Delete(T entity);
         void DeleteRange(IEnumerable<T> entities);
