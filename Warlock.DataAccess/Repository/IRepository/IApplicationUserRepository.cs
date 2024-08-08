22fa98342c27a254905d4f42f0c73b405543c7fc
nafis.exe <nafis.ali0123@gmail.com>
2024-08-09 09:59:20 +1000

    Add ApplicationUserRepository interface
    A new interface named IApplicationUserRepository has been added to the IRepository namespace. This interface extends from the base IRepository with ApplicationUser as its type.

---
+++ Warlock.DataAccess/Repository/IRepository/IApplicationUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warlock.Models;
+
+namespace Warlock.DataAccess.Repository.IRepository
+{
+    public interface IApplicationUserRepository : IRepository<ApplicationUser> { }
+}
