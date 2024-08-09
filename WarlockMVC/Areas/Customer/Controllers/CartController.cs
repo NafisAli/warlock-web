1ee3a12a933b27be1a05bdf3da7561a43c9bf8d9
nafis.exe <nafis.ali0123@gmail.com>
2024-08-09 10:01:31 +1000

    Add CartController in Customer area
    A new controller named 'CartController' has been added to the Customer area. This controller is responsible for managing the shopping cart operations such as viewing the cart, adding or reducing item quantity, and deleting items from the cart. It also includes a method to calculate price based on quantity. The controller uses an instance of IUnitOfWork to interact with the database.

---
+++ WarlockMVC/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,109 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Warlock.DataAccess.Repository.IRepository;
+using Warlock.Models;
+using Warlock.Models.ViewModels;
+
+namespace WarlockMVC.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(
+                    x => x.ApplicationUserId == userId,
+                    includeProperties: "Product"
+                )
+            };
+
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += (cart.Count * (double)cart.Price);
+            }
+
+            return View(ShoppingCartVM);
+        }
+
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId);
+
+            cartFromDb.Count += 1;
+
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId);
+
+            if (cartFromDb.Count <= 1)
+            {
+                _unitOfWork.ShoppingCart.Delete(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId);
+
+            _unitOfWork.ShoppingCart.Delete(cartFromDb);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+        {
+            if (shoppingCart.Count <= 50)
+            {
+                return shoppingCart.Product.Price;
+            }
+            else
+            {
+                if (shoppingCart.Count <= 100)
+                {
+                    return shoppingCart.Product.Price50;
+                }
+                else
+                {
+                    return shoppingCart.Product.Price100;
+                }
+            }
+        }
+    }
+}
