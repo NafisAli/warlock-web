388187cdab469c27184915cffcab9b9423306024
nafis.exe <nafis.ali0123@gmail.com>
2024-08-14 05:02:40 +1000

    Add payment status check in OrderConfirmation
    Added a new feature to the OrderConfirmation method in the CartController. Now, it checks if the order's payment status is not delayed. If so, it retrieves the session details and updates the StripePaymentID and Status of the order if the session's PaymentStatus is 'paid'. After these operations, all shopping cart items associated with that user are deleted.

--- WarlockMVC/Areas/Customer/Controllers/CartController.cs
+++ WarlockMVC/Areas/Customer/Controllers/CartController.cs
@@ -1,4 +1,4 @@
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Mono.TextTemplating;
@@ -236,6 +236,38 @@ namespace WarlockMVC.Areas.Customer.Controllers
 
         public IActionResult OrderConfirmation(int id)
         {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.Get(
+                x => x.Id == id,
+                includeProperties: "ApplicationUser"
+            );
+
+            if (orderHeader.PaymentStatus != SD.PaymentStatusDelayedPayment)
+            {
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId);
+
+                if (session.PaymentStatus == "paid")
+                {
+                    _unitOfWork.OrderHeader.UpdateStripePaymentID(
+                        id,
+                        session.Id,
+                        session.PaymentIntentId
+                    );
+                    _unitOfWork.OrderHeader.UpdateStatus(
+                        id,
+                        SD.StatusApproved,
+                        SD.PaymentStatusApproved
+                    );
+                    _unitOfWork.Save();
+                }
+            }
+
+            List<ShoppingCart> shoppingCarts = _unitOfWork
+                .ShoppingCart.GetAll(x => x.ApplicationUserId == orderHeader.ApplicationUserId)
+                .ToList();
+
+            _unitOfWork.ShoppingCart.DeleteRange(shoppingCarts);
+
             return View(id);
         }
 
