7868f822488a1bf23df7ca9da5b50ba5e71a7e1c
nafis.exe <nafis.ali0123@gmail.com>
2024-08-14 03:05:40 +1000

    Add OrderHeader and Details to DB
    This update introduces new tables for OrderHeaders and OrderDetails in the database. The OrderHeaders table includes fields such as Id, ApplicationUserId, OrderDate, ShippingDate, PaymentStatus, TrackingNumber among others. The OrderDetails table contains fields like Id, OrderHeaderId, ProductId, Count and Price.
    
    Additionally:
    - A column for 'OrderTotal' has been added to the 'OrderHeaders' table.
    - A new field 'State' has been introduced in the 'OrderHeaders' table.
    
    The changes also include necessary updates to the ApplicationDbContextModelSnapshot file reflecting these modifications.

--- Warlock.DataAccess/Migrations/20240813161113_AddOrderHeaderAndDetailsToDb.Designer.cs
+++ Warlock.DataAccess/Migrations/20240813161113_AddOrderHeaderAndDetailsToDb.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Warlock.DataAccess.Data;
 
 #nullable disable
 
-namespace WarlockMVC.Migrations
+namespace Warlock.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240813161113_AddOrderHeaderAndDetailsToDb")]
+    partial class AddOrderHeaderAndDetailsToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -392,9 +395,6 @@ namespace WarlockMVC.Migrations
                     b.Property<string>("OrderStatus")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<double>("OrderTotal")
-                        .HasColumnType("float");
-
                     b.Property<DateTime>("PaymentDate")
                         .HasColumnType("datetime2");
 
@@ -414,10 +414,6 @@ namespace WarlockMVC.Migrations
 
                     b.Property<DateTime>("ShippingDate")
                         .HasColumnType("datetime2");
-
-                    b.Property<string>("State")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("StreetAddress")
                         .IsRequired()
--- Warlock.DataAccess/Migrations/20240813162507_FixOrderTotalAccess.Designer.cs
+++ Warlock.DataAccess/Migrations/20240813162507_FixOrderTotalAccess.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Warlock.DataAccess.Data;
 
 #nullable disable
 
-namespace WarlockMVC.Migrations
+namespace Warlock.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240813162507_FixOrderTotalAccess")]
+    partial class FixOrderTotalAccess
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -414,10 +417,6 @@ namespace WarlockMVC.Migrations
 
                     b.Property<DateTime>("ShippingDate")
                         .HasColumnType("datetime2");
-
-                    b.Property<string>("State")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("StreetAddress")
                         .IsRequired()
