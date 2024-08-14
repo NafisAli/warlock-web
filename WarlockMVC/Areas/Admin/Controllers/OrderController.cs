ef9f9b1e14a6feae08fa3a08f41176c4621010e8
nafis.exe <nafis.ali0123@gmail.com>
2024-08-15 09:48:06 +1000

    Add OrderController in Admin area
    A new controller named OrderController has been added to the Admin area. This controller includes methods for viewing order details and fetching all orders based on their status. The controller interacts with the database through a unit of work, allowing it to retrieve order headers and details from the repository.

---
+++ WarlockMVC/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Warlock.DataAccess.Repository.IRepository;
+using Warlock.Models;
+using Warlock.Models.ViewModels;
+using Warlock.Utility;
+
+namespace WarlockMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int orderId)
+        {
+            OrderVM orderDetail =
+                new()
+                {
+                    OrderHeader = _unitOfWork.OrderHeader.Get(
+                        x => x.Id == orderId,
+                        includeProperties: "ApplicationUser"
+                    ),
+                    OrderDetails = _unitOfWork.OrderDetail.GetAll(
+                        x => x.OrderHeader.Id == orderId,
+                        includeProperties: "Product"
+                    )
+                };
+
+            return View(orderDetail);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork
+                .OrderHeader.GetAll(includeProperties: "ApplicationUser")
+                .ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(x =>
+                        x.PaymentStatus == SD.PaymentStatusPending
+                    );
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(x =>
+                        x.OrderStatus == SD.StatusInProcess
+                    );
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(x => x.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(x =>
+                        x.OrderStatus == SD.StatusApproved
+                    );
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = objOrderHeaders });
+        }
+
+        #endregion
+    }
+}
