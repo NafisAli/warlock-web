316b3da8fa72f5c83bf9b96ff8833aae1ca41949
nafis.exe <nafis.ali0123@gmail.com>
2024-08-16 05:55:28 +1000

    Add 'Cancelled' status to Warlock Utility
    A new constant string for the 'Cancelled' status has been added to the Warlock Utility. This will allow us to better track and manage orders that have been cancelled by users or administrators.

--- Warlock.Utility/SD.cs
+++ Warlock.Utility/SD.cs
@@ -18,6 +18,7 @@ namespace Warlock.Utility
         public const string StatusInProcess = "Processing";
         public const string StatusShipped = "Shipped";
         public const string StatusRefunded = "Refunded";
+        public const string StatusCancelled = "Cancelled";
 
         public const string PaymentStatusPending = "Pending";
         public const string PaymentStatusApproved = "Approved";
