70737ddf0ebb64911b24a05e6023d192bd299f9f
nafis.exe <nafis.ali0123@gmail.com>
2024-08-17 02:31:11 +1000

    Add session tracking to shopping cart
    The update introduces session tracking for the shopping cart in the Customer area. Now, when a user adds an item to their shopping cart, the count of items is stored in the user's session. This allows us to keep track of how many items a user has in their cart across multiple requests.

--- WarlockMVC/Areas/Customer/Controllers/HomeController.cs
+++ WarlockMVC/Areas/Customer/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Warlock.DataAccess.Repository.IRepository;
 using Warlock.Models;
+using Warlock.Utility;
 
 namespace WarlockMVC.Areas.Customer.Controllers
 {
@@ -64,6 +65,10 @@ namespace WarlockMVC.Areas.Customer.Controllers
             else
             {
                 _unitOfWork.ShoppingCart.Add(shoppingCart);
+                HttpContext.Session.SetInt32(
+                    SD.SessionCart,
+                    _unitOfWork.ShoppingCart.Get(x => x.ApplicationUserId == userId).Count
+                );
             }
 
             TempData["success"] = "Cart updated successfully";
