6f60c043ef6827489a0b69ff65a879b41553d1e1
nafis.exe <nafis.ali0123@gmail.com>
2024-08-17 07:25:54 +1000

    Add user management functionality in JS
    This commit introduces a new JavaScript file for managing users. It includes functions to load a data table with user information and toggle the lock status of a user. The data table displays various details about each user, including name, email, phone number, faction name, role and lockout end date. The 'ToggleLock' function sends an AJAX POST request to change the lock status of a selected user and updates the data table upon success.

---
+++ WarlockMVC/wwwroot/js/user.js
@@ -0,0 +1,65 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url:  '/admin/user/getall' },
+        "columns": [
+            { data: 'name', "width": "10%" },
+            { data: 'email', "width": "20%" },
+            { data: 'phoneNumber', "width": "10%" },
+            { data: 'faction.name', "width": "10%" },
+            { data: 'role', "width": "5%" },
+            {
+                data: { id: 'id', lockoutEnd: "lockoutEnd" },
+                "render": function (data) {
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+
+                    if (lockout > today) {
+                        return `
+                        <div class="text-center">
+                            <a onclick=ToggleLock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:120px;">
+                                <i class="bi bi-lock-fill"></i> Locked
+                            </a>
+                            <a href="/admin/user/RoleManagement?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
+                                <i class="bi bi-pencil-square"></i> Permission
+                            </a>
+                        </div>`
+                    } else {
+                        return `
+                        <div class="text-center">
+                            <a onclick=ToggleLock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:120px;">
+                                <i class="bi bi-unlock-fill"></i> Unlocked
+                            </a>
+                            <a href="/admin/user/RoleManagement?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
+                                <i class="bi bi-pencil-square"></i> Permission
+                            </a>
+                        </div>`
+                    }
+                },
+                "width": "30%"
+             }
+        ]
+    });
+}
+
+function ToggleLock(id) {
+    $.ajax({
+        type: "POST",
+        url: '/admin/user/togglelock',
+        data: JSON.stringify(id),
+        contentType: 'application/json',
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.ajax.reload();
+            } else {
+                toastr.error(data.message);
+            }
+        }
+    });
+}
