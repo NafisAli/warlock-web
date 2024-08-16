6cf841db524b558ff2b92564461b739d90107b77
nafis.exe <nafis.ali0123@gmail.com>
2024-08-17 07:25:11 +1000

    Add RoleManagementVM in Models
    Introduced a new ViewModel, RoleManagementVM, in the Models. This model includes ApplicationUser, RoleList and FactionList as required properties. The RoleList and FactionList are collections of SelectListItem for dropdowns.

---
+++ Warlock.Models/ViewModels/RoleManagementVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Warlock.Models.ViewModels
+{
+    public class RoleManagementVM
+    {
+        public required ApplicationUser ApplicationUser { get; set; }
+        public required IEnumerable<SelectListItem> RoleList { get; set; }
+        public required IEnumerable<SelectListItem> FactionList { get; set; }
+    }
+}
